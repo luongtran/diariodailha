bae6626df606a621e1c5fb0031a5caa216e990b4
Raphael de Andrade <rhdeandrade@gmail.com>
2012-10-18 17:49:51 -0300

    Repo Iniciado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diariodailha::Application.config.secret_token = '1ff9b1827884a21a48eb1991cda8335e9a93e2e6a3ea5f1143e4d38a72658ec5f25accc6d305f4560470c006834a3228b4da7306a267fecb97719e302f7169c6'
