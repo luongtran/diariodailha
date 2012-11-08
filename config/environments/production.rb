59ab5172617c97bea4f8b6a595024800c3292c9f
Derik Evangelista <kirederik@gmail.com>
2012-11-08 10:29:40 -0200

    Prod email
    Signed-off-by: Derik Evangelista <kirederik@gmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,6 +53,17 @@ Diariodailha::Application.configure do
 
   # Enable threaded mode
   # config.threadsafe!
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "contato.diariodailha@gmail.com",
+    password: "diariodailha"
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
