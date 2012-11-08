be09b2ec6e9b82b5fa2ac891e10bb5a42a9f952a
Raphael de Andrade <rhdeandrade@gmail.com>
2012-11-08 01:43:52 -0200

    Added assets to compile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require jquery-fileupload/basic
 //= require jquery-fileupload/vendor/tmpl
+//= require jquery.ui.core
 //= require jquery.ui.datepicker
 //= require_tree .
 //= require ckeditor/init
