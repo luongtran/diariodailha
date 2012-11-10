8aa6098c09559a89752749cef690dc6a92487faf
Raphael de Andrade <rhdeandrade@gmail.com>
2012-11-10 18:17:55 -0200

    Translated pag of contents

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,3 +1,6 @@
 class Content < ActiveRecord::Base
   attr_accessible :body, :description, :name, :subtitle, :title
+
+  validates_uniqueness_of :name
+  validates_presence_of :description, :name, :subtitle, :title
 end
