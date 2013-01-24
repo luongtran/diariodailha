26601dc4e3ce6de6d041cc567de9228d7ff168cb
Raphael de Andrade <rhdeandrade@gmail.com>
2013-01-23 22:12:02 -0200

    Changed description of photos

--- db/schema.rb
+++ db/schema.rb
@@ -100,6 +100,13 @@ ActiveRecord::Schema.define(:version => 20121208200447) do
     t.boolean  "contrast"
   end
 
+  create_table "news", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "news_photos", :force => true do |t|
     t.string   "image"
     t.string   "legend"
